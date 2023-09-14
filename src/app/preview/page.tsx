34d433ae0915fb28fac855621072a160552b573b
thanh.bui <thanh.bui@niteco.se>
2023-09-14 18:53:28 +0700

    Add preview page

---
+++ src/app/preview/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import FlipCard from "@/components/FlipCard";
+import Header from "@/components/Header";
+import { LMContainer } from "@/components/LMContainer";
+import { loadState } from "@/utils";
+import { useEffect, useMemo, useState } from "react";
+import styled from "styled-components";
+
+const Result = () => {
+  return (
+    <main>
+      <iframe
+        title="eKoin"
+        src={"http://hei-ekoin-dev-frontend.azurewebsites.net/test-link"}
+        allowFullScreen
+        allow="camera;accelerometer;gyroscope"
+        width="100%"
+        style={{
+          height: "100vh",
+        }}
+        frameBorder="0"
+        // sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts"
+      />
+    </main>
+  );
+};
+
+export default Result;
